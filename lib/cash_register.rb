67b077562291e1e30ac33ef9238bd21059601b8c
Maria Nunez <mariaalician@gmail.com>
2018-07-30 23:08:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister
+  attr_accessor :total, :discount, :title, :price
+  
+  def initialize(total = 0)
+    @total = total
+  end
+  
+  def cash_register_with_discount(discount)
+    discount = @total*0.8
 
+  end
+  
+  def add_item(title, price)
+  
+  end
+
+end
